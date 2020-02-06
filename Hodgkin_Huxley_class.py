bc98fbbc9b8a6af413c80dd9b5e200c172c3d412
Pavan Chaggar <pavanchaggar@Pavans-MacBook-Pro.local>
2020-02-06 14:24:27 +0000

    more changes

--- Hodgkin_Huxley_class.py
+++ Hodgkin_Huxley_class.py
@@ -35,23 +35,23 @@ class Hodgkin_Huxley:
         self.step_current = np.piecewise(self.time, [(self.time< 500)*(self.time>=200), (self.time< 950)*(self.time>=700)], [10,50])
 
     # potassium rate functions
-    def _a_n(self):
+    def a_n(self):
         return 0.01 * (self.V_m + 55) / (1 - np.exp(-(self.V_m + 55.0) / 10)) # + 1.E-7)
 
-    def _B_n(self):
+    def B_n(self):
         return 0.125 * np.exp(-(self.V_m + 65.0) / 80)
 
     # sodium rate functions
-    def _a_m(self):
+    def a_m(self):
         return 0.1 * (40 + self.V_m) / (1.0 - np.exp(-(self.V_m + 40.0) / 10.0)) # + 1.E-7)
 
-    def _a_h(self):
+    def a_h(self):
         return 0.07 * np.exp(-(self.V_m + 65.0) / 20.0)
 
-    def _B_m(self):
+    def B_m(self):
         return 4 * np.exp(-(self.V_m + 65.0) / 18.0)
 
-    def _B_h(self):
+    def B_h(self):
         return 1 / (np.exp(-(35 + self.V_m) / 10) + 1)
 
     # current functions
@@ -61,10 +61,10 @@ class Hodgkin_Huxley:
     def Na_current(self):
         self.Na_current = self.g_Na * self.m**3 * self.h * (self.V_m - self.V_Na)
 
-    def leak_current(selfl):
+    def leak_current(self):
         self.leakcurrent = self.g_l * (self.V_m - self.V_l)
 
-    def hh_model(y, t, parameters):
+    def hh_model(self, y, t, parameters):
         '''Takes in y dynamics variables, V_m, n, m and h. Array of time steps to integrate over.
         List of constant parameters, reversal potentials and conductances
 
@@ -74,21 +74,21 @@ class Hodgkin_Huxley:
         self.g_K, self.V_K, self.g_Na, self.V_Na, self.g_l, self.C_m = parameters
 
         # Total current through the membrane
-        self.dVdt = self.I_inj(t) - self.K_current(self.g_K,self.n,self.V_m,Vself._K) - self.Na_current(self.g_Na, self.m, self.h, self.V_m, self.V_Na) - self.leak_current(self.g_l, self.V_m, self.V_l) / self.C_m
+        self.dVdt = self.I_inj() - self.K_current() - self.Na_current() - self.leak_current() / self.C_m
 
         # Derivative of n, potassium channel activation, w.r.t. time
-        self.dndt = self.a_n(self.V_m) * (1 - self.n) - self.B_n(self.V_m) * self.n
+        self.dndt = self.a_n() * (1 - self.n) - self.B_n() * self.n
 
         # Derivative of m, sodium channel activion, w.r.t. time
-        self.dmdt = self.a_m(self.V_m) * (1 - self.m) - self.B_m(self.V_m) * self.m
+        self.dmdt = self.a_m() * (1 - self.m) - self.B_m() * self.m
 
         # Derivative of h, sodium channel in-activion, w.r.t. time
 
-        self.dhdt = self.a_h(self.V_m) * (1 - self.h) - self.B_h(self.V_m) * self.h
+        self.dhdt = self.a_h() * (1 - self.h) - self.B_h() * self.h
 
     def simulate(self):
 
         y0 = [self.V_m, self.n, self.m, self.h]
         parameters = [self.g_K, self.V_K, self.g_Na, self.V_Na, self.g_l, self.C_m]
 
-        self.sol = odeint(hh_model,y0, time, args=(parameters,))
+        self.sol = odeint(self.hh_model, y0, self.time, args=(parameters,))
